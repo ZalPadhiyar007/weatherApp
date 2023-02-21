be18d6f86e2aa859a47ab9767f24d753ab410390
Zal Padhiyar <zalakpadhiyar@zals-air.home>
2023-02-21 07:22:02 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMAP: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "4eeccfbc5c6f54dd4ae4ccd27bdd70ae"
+    }
+}
+
+module.exports = constants;
